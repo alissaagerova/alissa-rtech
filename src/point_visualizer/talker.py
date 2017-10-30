fad50df4b6a552d50cee57fcaaa4ba6c69b3fa0b
Tudeng <alissa.agerova@gmail.com>
2017-10-30 11:59:47 +0200

    New talker.py

---
+++ src/point_visualizer/talker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from geometry_msgs.msg import PointStamped
+def talker():
+    pub = rospy.Publisher('chatter', PointStamped, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+        point = PointStamped()
+	point.point.x=1
+	point.point.y=2
+    while not rospy.is_shutdown():
+
+        pub.publish(point)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
