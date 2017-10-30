a9598de9636b1363731068be77fed19907dd68e6
Choco83 <asif.sattar93@gmail.com>
2017-10-30 22:54:32 +0200

    lab7_update

--- src/point_visualizer/scripts/talker.py
+++ src/point_visualizer/scripts/talker.py
@@ -2,17 +2,21 @@
 # license removed for brevity
 import rospy
 from geometry_msgs.msg import PointStamped
+
+
 def talker():
     pub = rospy.Publisher('chatter', PointStamped, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-	point = PointStamped()
-	point.point.x=1
-	point.point.y=2
+    rate = rospy.Rate(10)  # 10hz
+    point = PointStamped()
+    point.point.x = 1
+    point.point.y = 2
+    point.header.frame_id="base_link"
     while not rospy.is_shutdown():
 
         pub.publish(point)
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
